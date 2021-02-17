af8fd415b4259e5ba21250994091b906ee281e06
velcronator <valacos7@gmail.com>
2021-02-18 00:14:43 +1100

    Intial Commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVal = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zVal = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xVal, 0, zVal);
+    }
+}
